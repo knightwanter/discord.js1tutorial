fce891cffe7eb7a360b72935a20a17ef334f13d7
Knight wanter <76393623+knightwanter@users.noreply.github.com>
2021-05-27 21:24:08 +0530

    Create uptime.js

---
+++ commands/uptime.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js);
+                        
+                        
+module.exports = {
+  name: "uptime",
+  description: "Check the uptime"
+  execute(client, message, args) {
+    let seconds = Math.floor(client.uptime / 1000);
+    let minutes = Math.floor(seconds / 60);
+    let hours = Math.floor(minutes / 60);
+    let days = Math.floor(hours / 24);
+
+    seconds %= 60;
+    minutes %= 60;
+    hours %= 24;
+
+    return message.channel.send(`**Uptime: \`${days} day(s),${hours} hours, ${minutes} minutes, ${seconds} seconds\`**`)
+      .catch(console.error);
+  }
+};
